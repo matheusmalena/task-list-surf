50b536bee816bd15e0ad36851bc1d0a9bbebdcea
Matheus Malena <matheus@tew.one>
2024-05-24 17:59:27 -0300

    update

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,127 +1,21 @@
 <template>
-  <section class="d-flex justify-content-center flex-column align-items-center">
+  <div class="projetos">
     <h1>Projetos</h1>
-    <form
-      @submit.prevent="salvar"
-      class="d-flex flex-column justify-content-center align-items-center gap-3 w-100"
-    >
-      <div
-        class="d-flex flex-column justify-content-center align-items-center gap-2 w-100"
-      >
-        <label for="nomeDoProjeto"> Nome do Projeto</label>
-        <input type="text" v-model="nomeDoProjeto" id="nomeDoProjeto" />
-      </div>
-      <div>
-        <button class="button-save" type="submit">Salvar</button>
-      </div>
-    </form>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+  </div>
+  <router-view></router-view>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent } from "vue";
-import { useStore } from "@/store";
+<script lang="ts" >
+import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: "Projetos",
-  data() {
-    return {
-      nomeDoProjeto: "",
-    };
-  },
-  methods: {
-    salvar() {
-      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-      this.nomeDoProjeto = "";
-    },
-  },
-  setup(){
-    const store = useStore()
-    return {
-        store,
-        projetos: computed(() => store.state.projetos)
-    }
-  }
-});
+export default defineComponent ({
+  name: 'Projetos',
+})
+
 </script>
 
 <style scoped>
-.table {
-  padding: 1.5rem;
-  border-radius: 10px;
-  width: 80%;
-  margin: 2rem;
-}
-
-thead tr {
-  border-bottom: 1px solid #598e98;
-}
-
-tbody {
-    width: 100%;
-}
-
-thead{
-    width: 100%;
-}
-
-tbody tr td{
-    border: none;
-}
-
-tbody tr {
-  display: flex; 
-  flex-direction: row;
-  justify-content: space-between;
-  border: 1px solid black; /* Adiciona borda completa ao <tr> */
-  margin-bottom: 1rem; /* Espaçamento entre as linhas */
-  width: 100%;
-  padding: 1rem;
-  border-radius: 6px;
-  margin-top: 1rem;
-}
-
-label {
-  font-size: 1.15rem;
-}
-
-input {
-  width: 40%;
-  padding: 0.5rem;
-  border-radius: 6px;
-}
-
-.button-save {
-  background-color: #598e98;
-  padding: 0.5rem;
-  color: white;
-  border-radius: 6px;
-  border: 1px solid black;
-}
-
-form {
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  padding: 1.5rem;
-}
-
-@media screen and (max-width: 760px) {
-  input {
-    width: 60%;
-    padding: 0.4rem;
+  .projetos{
+    text-align: center;
   }
-}
 </style>
---
+++ src/views/Projetos/Lista.vue
@@ -0,0 +1,175 @@
+<template>
+  <section class="d-flex justify-content-center flex-column align-items-center">
+    <router-link to="/projetos/novo" class="button">
+      <span class="material-symbols-outlined icon-more">add_circle</span>
+      <span>Novo projeto</span>
+    </router-link>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
+          <td>
+            <router-link :to="`/projetos/${projeto.id}`">
+              <span class="material-symbols-outlined icon-edit">edit</span>
+            </router-link>
+            <button class="delete" @click="excluir(projeto.id)">
+              <span class="material-symbols-outlined"> delete </span>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+import { useStore } from "@/store";
+import { EXCLUIR_PROJETO } from "@/store/tipo-mutacoes";
+
+export default defineComponent({
+  name: "Lista",
+  methods: {
+    excluir(id: string){
+        this.store.commit(EXCLUIR_PROJETO, id)
+        console.log("Tarefa deletada!")
+    }
+  },
+  setup() {
+    const store = useStore();
+    return {
+      projetos: computed(() => store.state.projetos),
+      store
+    };
+  },
+});
+</script>
+
+<style scoped>
+.table {
+  width: 80%;
+  margin: 2rem auto;
+  border-collapse: collapse; /* Alterado para collapse */
+}
+
+thead,
+tbody tr {
+  display: table; /* Faz com que se comportem como uma linha de tabela */
+  width: 100%; /* Garante que ocupem o mesmo espaço */
+  table-layout: fixed; /* Ajuda a manter colunas de largura igual */
+}
+
+thead tr,
+tbody tr {
+  border-bottom: 1px solid #598e98; /* Aplica borda de forma consistente */
+}
+
+thead {
+  margin-bottom: 0.5rem; /* Adiciona espaçamento entre thead e tbody */
+}
+
+thead tr {
+  border-bottom: 1px solid #598e98;
+}
+
+tbody {
+  display: flex; /* Permite definir um espaçamento entre thead e tbody */
+  justify-content: space-between;
+  flex-direction: column;
+  width: 100%;
+}
+
+tbody tr {
+  border-radius: 6px; /* Mantém o estilo de borda arredondada */
+  margin-top: 1rem; /* Espaçamento entre linhas */
+  background-color: #fff; /* Fundo branco para as linhas */
+  border: none;
+  margin-top: 1rem;
+  border: 1px solid #a4a4a4;
+  border-radius: 6px;
+}
+
+tbody tr td {
+  padding: 1rem; /* Espaço interno para as células */
+}
+
+/* tbody tr {
+    display: flex;
+    width: 100%;
+    padding: 1rem; 
+    box-sizing: border-box;
+    border: 1px solid black;
+    border-radius: 6px;
+    margin-top: 1rem;
+  } */
+
+tbody tr td {
+  border: none;
+  flex: 1;
+  width: 32%;
+}
+
+label {
+  font-size: 1.15rem;
+}
+
+input {
+  width: 40%;
+  padding: 0.5rem;
+  border-radius: 6px;
+}
+
+.button-save {
+  background-color: #598e98;
+  padding: 0.5rem;
+  color: white;
+  border-radius: 6px;
+  border: 1px solid black;
+}
+
+form {
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 1.5rem;
+}
+
+.button {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  background-color: #d4bfa3;
+  padding: 0.5rem;
+  color: black;
+  text-decoration: none;
+  border-radius: 6px;
+}
+
+.icon-edit {
+  color: black;
+}
+
+.delete{
+    background-color: transparent;
+    border: none;
+    text-decoration: none;
+}
+
+.delete span{
+    background: transparent;
+    color: red;
+}
+
+@media screen and (max-width: 760px) {
+  input {
+    width: 60%;
+    padding: 0.4rem;
+  }
+}
+</style>

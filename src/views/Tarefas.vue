b1f5db5b0fb7785b0f7f1133ed3f013d70148500
Matheus Malena <matheus@tew.one>
2024-09-11 17:09:09 -0300

    add filter

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="d-flex row w-100" >
+  <div class="d-flex row w-100">
     <Formulario @aoSalvarTarefa="salvarTarefa" />
   </div>
   <div class="list-group gap-4 m-5">
-    <div class="div-table" >
+    <div class="div-table">
+      <div class="card-filter" >
+        <font-awesome-icon :icon="['fas', 'filter']" class="filter-icon" />
+      <input
+        type="text"
+        v-model="filtro"
+        placeholder="Filtrar..."
+        class="form-control filter "
+      />
+    </div>
       <table>
         <thead>
           <tr>
@@ -15,7 +24,7 @@
         </thead>
       </table>
       <h1>Tarefas</h1>
-      <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+      <Tarefa v-for="(tarefa, index) in tarefasFiltradas" :key="index" :tarefa="tarefa" />
     </div>
     <Box v-if="listaEstaVazia" class="box-nodata box">
       <strong> Você não está muito produtivo hoje</strong>
@@ -25,7 +34,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "vue";
+import { defineComponent, computed, ref } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
@@ -43,21 +52,30 @@ export default defineComponent({
   },
   setup() {
     const store = useStore();
+    const filtro = ref("");
 
     const tarefas = computed(() => store.state.tarefas);
-    const listaEstaVazia = computed(() => tarefas.value.length === 0);
+
+    const tarefasFiltradas = computed(() =>
+      tarefas.value.filter((tarefa) =>
+        tarefa.descricao.toLowerCase().includes(filtro.value.toLowerCase())
+      )
+    );
+
+    const listaEstaVazia = computed(() => tarefasFiltradas.value.length === 0);
 
     const salvarTarefa = (tarefa: ITarefa) => {
       store.commit(ADICIONA_TAREFA, tarefa);
       store.commit(NOTIFICAR, {
-          titulo: 'Tarefa salva!',
-          texto: 'Sua tarefa foi adicionada na lista!',
-          tipo: TipoNotificacao.SUCESSO
-        })
+        titulo: "Tarefa salva!",
+        texto: "Sua tarefa foi adicionada na lista!",
+        tipo: TipoNotificacao.SUCESSO,
+      });
     };
 
     return {
-      tarefas,
+      filtro,
+      tarefasFiltradas,
       listaEstaVazia,
       salvarTarefa,
     };
@@ -66,12 +84,9 @@ export default defineComponent({
 </script>
 
 <style scoped>
-h1{
-    color: #598e98;
-    font-weight: 600;
-    font-family: "Poppins", sans-serif;
-    text-align: center;
-  }
+h1 {
+  display: none;
+}
 
 .box strong {
   color: rgb(99, 99, 99) !important;
@@ -103,9 +118,37 @@ table {
   border-bottom: 3px solid #598e98;
 }
 
+.filter {
+  width: 25%;
+}
+
+.card-filter {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.filter-icon {
+  color: #0e4e68;
+  font-size: 22px;
+  text-align: center;
+}
+
 @media screen and (max-width: 768px) {
   thead {
     display: none;
   }
+  h1 {
+    display: block;
+    color: #598e98;
+    font-weight: 600;
+    font-family: "Poppins", sans-serif;
+    text-align: center;
+  }
+
+  .filter {
+  width: 100%;
+} 
 }
 </style>
